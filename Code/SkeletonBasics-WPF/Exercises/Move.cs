5bd62594bae37e7b6963c829dcef782f301d8576
Megan Quintero <mmquint@college.harvard.edu>
2013-11-04 19:37:53 -0500

    adding skeleton code

---
+++ Code/SkeletonBasics-WPF/Exercises/Move.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics.Exercises
+{
+    class Move
+    {
+        enum Moves
+        {
+            jumpingJacks,
+            burpees,
+            highJumps
+        };
+
+        string Name;
+        Moves Type;
+
+        public Move(int type)
+        {
+            Type = (Moves)type;
+            Name = Type.ToString();
+        }
+    }
+}
